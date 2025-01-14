73350ce52149391aa85ae8a7aeed76fb469be9b2
Viktor Svalstedt <viktor.svalan@gmail.com>
2025-01-14 14:23:55 +0100

    done

--- help_for_noobs.py
+++ help_for_noobs.py
@@ -7,4 +7,8 @@ df['result'].value_counts(dropna=False)
 
 import matplotlib.pyplot as plt
 
+X = pd.Series()
 plt.ecdf(X) #KEEEEEEYYYYYYYYYYYYY
+
+path= str()
+data = pd.read_csv(path, on_bad_lines='warn')
